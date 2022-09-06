33daee1514831840cb9da5e32338579bb275e151
Home <1984milu@mail.ru>
2022-09-06 14:20:31 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setLastnameFirstnamePatronymic("Kozlow Nikolay Olegovich");
+        student.setGroup("Biology");
+        student.setEntrance(new Date());
+
+        System.out.println(student.getLastnameFirstnamePatronymic()
+                + " joined the group "
+                + student.getGroup()
+                + " " + student.getEntrance());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String lastnameFirstnamePatronymic;
+    private String group;
+    private Date entrance;
+
+    public String getLastnameFirstnamePatronymic() {
+        return lastnameFirstnamePatronymic;
+    }
+
+    public void setLastnameFirstnamePatronymic(String lastnameFirstnamePatronymic) {
+        this.lastnameFirstnamePatronymic = lastnameFirstnamePatronymic;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(Date entrance) {
+        this.entrance = entrance;
+    }
+}
