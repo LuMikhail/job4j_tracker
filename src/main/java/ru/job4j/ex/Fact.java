4c2a47b46489accca98024ff83b463f0691cc2ad
Home <1984milu@mail.ru>
2022-09-20 15:41:54 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #345258]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public  int calc(int n) throws IllegalAccessException {
+    public  int calc(int n) {
         if (n < 0) {
-                throw new IllegalAccessException("N could not be less then 0");
-            }
+            throw new IllegalArgumentException("Start should be less than finish.");
+        }
         int rsl = 1;
         for (int index = 0; index <= n; index++) {
             rsl *= index;
@@ -12,7 +12,7 @@ public class Fact {
         return rsl;
     }
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) {
         new Fact().calc(-1);
     }
 }
