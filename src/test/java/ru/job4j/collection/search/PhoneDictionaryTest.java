abb3219fd4b267779df8586af5c1514be65f64b8
Home <1984milu@mail.ru>
2022-10-12 13:48:21 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/test/java/ru/job4j/collection/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -15,6 +15,14 @@ class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("534");
         assertThat(persons.get(0).getName()).isEqualTo("Petr");
+    }
+
+    @Test
+    public void whenFindByNothing() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
         ArrayList<Person> empty = new ArrayList<>();
         ArrayList<Person> personsNotFound = phones.find("89");
         assertThat(personsNotFound).isEqualTo(empty);
