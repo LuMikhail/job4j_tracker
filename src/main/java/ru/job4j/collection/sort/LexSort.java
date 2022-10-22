b25228bbbb036c93c3853761891b4365381c43d2
Home <1984milu@mail.ru>
2022-10-22 11:19:26 +0300

    3. Компаратор для строк [#35008]

--- src/main/java/ru/job4j/collection/sort/LexSort.java
+++ src/main/java/ru/job4j/collection/sort/LexSort.java
@@ -8,9 +8,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] right = o1.split("\\.");
         String[] left = o2.split("\\.");
-        if (right.length == 0 || left.length == 0) {
-            return 0;
-        }
         return Integer.compare(Integer.parseInt(right[0]), Integer.parseInt(left[0]));
     }
 }
