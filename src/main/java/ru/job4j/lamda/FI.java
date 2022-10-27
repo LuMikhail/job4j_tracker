77e18dc379a9f0a04946a3df9c308e810af8a713
Home <1984milu@mail.ru>
2022-10-27 11:34:47 +0300

    2.0. Лямбда [#58165]

---
+++ src/main/java/ru/job4j/lamda/FI.java
@@ -0,0 +1,35 @@
+package ru.job4j.lamda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Класс сравнивает объекты и сортирует их с использованием лямбда выражений:
+ * <p>
+ * 1. Сортировка по возрастанию размера.
+ * <p>
+ * 2. Сортирует строки по возрастанию.
+ * <p>
+ * 3. Сортирует строки по убыванию длины.
+ *
+ * @version 1.0
+ */
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1.", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
+        Arrays.sort(atts, comparator);
+        System.out.println(Arrays.toString(atts));
+        Comparator<Attachment> decreaseSort = (left, right) -> left.getName().compareTo(right.getName());
+        Arrays.sort(atts, decreaseSort);
+        System.out.println(Arrays.toString(atts));
+        Comparator<Attachment> decreaseSortLength = (left, right)
+                -> Integer.compare((left.getName().length()), right.getName().length());
+        Arrays.sort(atts, decreaseSortLength);
+        System.out.println(Arrays.toString(atts));
+    }
+}
