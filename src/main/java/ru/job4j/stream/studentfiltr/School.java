be2071675e80f77f7d9ebcc0fe14607df232f3c9
Home <1984milu@mail.ru>
2022-10-30 21:32:07 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/studentfiltr/School.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream.studentfiltr;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Класс с помощью метода collect создает новую коллекцию, в соответствии с условием лямбда-выражения.
+ * Условия лямбда-выражений определены в классе SchoolTest.
+ *
+ * @version 1.0
+ */
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
+                .filter(t -> predict.test(t))
+                .collect(Collectors.toList());
+    }
+}
