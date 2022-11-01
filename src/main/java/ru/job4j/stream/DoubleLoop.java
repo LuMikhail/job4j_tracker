43a226b6d80fda96f7ecfc84fb523a22e132bb6f
Home <1984milu@mail.ru>
2022-11-01 23:03:58 +0300

    7. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

---
+++ src/main/java/ru/job4j/stream/DoubleLoop.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * Класс показывает работу stream и for
+ */
+public class DoubleLoop {
+    public static void main(String[] args) {
+        String[] levels = {"level 1", "level 2", "level 3"};
+        String[] tasks = {"task 1", "task 2", "task 3", "task 4"};
+        Stream.of(levels)
+                .flatMap(level -> Stream.of(tasks)
+                        .map(task -> level + " " + task))
+                .forEach(System.out::println);
+        System.out.println();
+
+        for (String level : levels) {
+            for (String task : tasks) {
+                System.out.println(level + " " + task);
+            }
+        }
+    }
+}
