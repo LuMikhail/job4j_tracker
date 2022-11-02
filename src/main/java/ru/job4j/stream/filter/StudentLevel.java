354be012cc5d3026ccfe17dda9eea82558534876
Home <1984milu@mail.ru>
2022-11-02 23:10:19 +0300

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/filter/StudentLevel.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream.filter;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Класс позволяет с помощью метода получить отсортированную коллекцию с заданным условием без null объектов.
+ *
+ * @version 1.0
+ */
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted(Comparator.comparingInt(Student::getScore).reversed())
+                .takeWhile(student -> student.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
