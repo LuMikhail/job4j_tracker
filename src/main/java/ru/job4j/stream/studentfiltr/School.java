13883be30c4a0b5c9f12885bb83e0391ee1fd743
Home <1984milu@mail.ru>
2023-01-25 14:11:44 +0300

    1. Sql схема для проекта Tracker. [#276987]

--- src/main/java/ru/job4j/stream/studentfiltr/School.java
+++ src/main/java/ru/job4j/stream/studentfiltr/School.java
@@ -3,6 +3,7 @@ package ru.job4j.stream.studentfiltr;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Класс с помощью метода collect создает новую коллекцию, в соответствии с условием лямбда-выражения.
